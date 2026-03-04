b81510d7140d3c8977923c0570996690bdcf2941
v0id <>
2026-03-04 10:56:47 +0200

    Revert "Fix crash"
    This reverts commit b82b3ade7b4f7b55b3ffe4a8d7329fc546d9bf67.

--- Content.Goobstation.Server/MisandryBox/Thunderdome/ThunderdomeRuleSystem.cs
+++ Content.Goobstation.Server/MisandryBox/Thunderdome/ThunderdomeRuleSystem.cs
@@ -28,7 +28,6 @@ using Robust.Server.GameStates;
 using Robust.Server.Player;
 using Robust.Shared.Audio;
 using Robust.Shared.Configuration;
-using Robust.Shared.Enums;
 using Robust.Shared.Map;
 using Robust.Shared.Player;
 using Robust.Shared.Random;
@@ -76,24 +75,6 @@ public sealed class ThunderdomeRuleSystem : EntitySystem
         SubscribeLocalEvent<ThunderdomeOriginalBodyComponent, MobStateChangedEvent>(OnOriginalBodyStateChanged);
         SubscribeNetworkEvent<ThunderdomeRevivalAcceptEvent>(OnRevivalAccept);
         SubscribeLocalEvent<ThunderdomePlayerComponent, SuicideGhostEvent>(OnSuicideAttempt);
-
-        // If a player disconnects while their loadout EUI is open, keep our tracking clean so round cleanup
-        // doesn't try to close an EUI for a session that no longer exists in EUI manager state.
-        _playerManager.PlayerStatusChanged += OnPlayerStatusChanged;
-    }
-
-    public override void Shutdown()
-    {
-        base.Shutdown();
-        _playerManager.PlayerStatusChanged -= OnPlayerStatusChanged;
-    }
-
-    private void OnPlayerStatusChanged(object? sender, SessionStatusEventArgs e)
-    {
-        if (e.NewStatus != SessionStatus.Disconnected)
-            return;
-
-        _activeEuis.Remove(e.Session);
     }
 
     public override void Update(float frameTime)
@@ -124,11 +105,8 @@ public sealed class ThunderdomeRuleSystem : EntitySystem
 
     private void OnRoundEnding(RoundRestartCleanupEvent ev)
     {
-        foreach (var eui in _activeEuis.Values.ToArray())
-        {
-            if (eui.Player.Status != SessionStatus.Disconnected)
-                eui.Close();
-        }
+        foreach (var eui in _activeEuis.Values)
+            eui.Close();
         _activeEuis.Clear();
 
         if (_ruleEntity == null)
